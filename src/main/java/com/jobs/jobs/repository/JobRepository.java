6f0469cf024ac51c2a443fd2ead2dbe8608d3a0b
sophiesucode <sophzsul@gmail.com>
2022-02-25 22:05:16 -0500

    added methods to find by name

--- src/main/java/com/jobs/jobs/repository/JobRepository.java
+++ src/main/java/com/jobs/jobs/repository/JobRepository.java
@@ -4,4 +4,22 @@ import com.jobs.jobs.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+
+    Job findByJob_title(String name);
+
+
 }
+
+
+
+//    Properties findByCity(String city);
+//
+//    Properties findByStreet(String street);
+//
+//    Properties getPropertiesByIdAndAgent_Id(Long propertiesId , Long agentId);
+//
+//    Properties getPropertiesByRealEstateOfficeId(Long officeId);
+//
+//    Properties getPropertiesById(Long propertiesId);
+//
+//    Properties findByNeighborhood(String neighborhood);
