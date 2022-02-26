eeeb7639da66ec60d3ea69a04063d1b8e3808676
sophiesucode <sophzsul@gmail.com>
2022-02-25 21:44:59 -0500

    injected job and category repository into Category service

--- src/main/java/com/jobs/jobs/service/CategoryService.java
+++ src/main/java/com/jobs/jobs/service/CategoryService.java
@@ -1,5 +1,23 @@
 package com.jobs.jobs.service;
 
+import com.jobs.jobs.repository.CategoryRepository;
+import com.jobs.jobs.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CategoryService {
-    
+
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public void setCategoryRepository(CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    private JobRepository jobRepository;
+
+    @Autowired
+    public void setJobRepository(JobRepository jobRepository){
+        this.jobRepository = jobRepository;
+    }
 }
