0e7a0ead605782651b6cace1f38313bccfbd0480
sophiesucode <sophzsul@gmail.com>
2022-02-25 19:05:38 -0500

    re-adding files

---
+++ src/main/java/com/jobs/jobs/model/Category.java
@@ -0,0 +1,52 @@
+package com.jobs.jobs.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="categories")
+public class Category {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+
+    public Category() {
+
+    }
+
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/jobs/jobs/model/Employer.java
@@ -0,0 +1,47 @@
+package com.jobs.jobs.model;
+
+import javax.persistence.Entity;
+
+
+public class Employer {
+
+        private Long id;
+
+        private String company_name;
+
+
+        public Employer() {
+
+        }
+
+        public Employer(Long id, String company_name) {
+            this.id = id;
+            this.company_name = company_name;
+        }
+
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getCompany_name() {
+            return company_name;
+        }
+
+        public void setCompany_name(String company_name) {
+            this.company_name = company_name;
+        }
+
+        @Override
+        public String toString() {
+            return "Employer{" +
+                    "id=" + id +
+                    ", company_name='" + company_name + '\'' +
+                    '}';
+        }
+    }
+
