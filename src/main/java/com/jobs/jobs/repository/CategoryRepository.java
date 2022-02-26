1fd1f5479be73b1fe7655acdb4f7f7127c3a674d
sophiesucode <sophzsul@gmail.com>
2022-02-25 21:38:14 -0500

    created a find category by name method in category repo

--- src/main/java/com/jobs/jobs/repository/CategoryRepository.java
+++ src/main/java/com/jobs/jobs/repository/CategoryRepository.java
@@ -2,6 +2,9 @@ package com.jobs.jobs.repository;
 
 import com.jobs.jobs.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String categoryName);
 }
--- src/main/java/com/jobs/jobs/service/CategoryService.java
+++ src/main/java/com/jobs/jobs/service/CategoryService.java
@@ -1,4 +1,5 @@
 package com.jobs.jobs.service;
 
 public class CategoryService {
+    
 }
