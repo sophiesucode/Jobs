3020b4bc3787ccb68d0bc823daff52a220a3a7ee
sophiesucode <sophzsul@gmail.com>
2022-02-27 20:06:19 -0500

    added request mapping and all api endpoints with full crud function

--- src/main/java/com/jobs/jobs/controller/CategoryController.java
+++ src/main/java/com/jobs/jobs/controller/CategoryController.java
@@ -1,4 +1,59 @@
 package com.jobs.jobs.controller;
 
+import com.jobs.jobs.model.Category;
+import com.jobs.jobs.model.Employer;
+import com.jobs.jobs.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
 public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryService getCategoryService() {
+        return categoryService;
+    }
+    @Autowired
+    public void setCategoryService(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+    @GetMapping("/categories")
+    public List<Category> getCategories() {
+        System.out.println("calling getCategories ==>");
+        return categoryService.getAllCategories();
+    }
+
+
+    @GetMapping(path = "/categories/{categoryId}")
+    public Optional getCategory(@PathVariable Long categoryId) {
+        System.out.println("calling getCategory ==>");
+        return categoryService.getCategory(categoryId);
+    }
+
+
+    @PostMapping("/categories/")
+    public Category createCategory(@RequestBody Category categoryObject) {
+        System.out.println("calling createCategory ==>");
+        return categoryService.createCategory(categoryObject);
+    }
+
+
+    @PutMapping("/categories/{categoryId}")
+    public Category updateCategory(@PathVariable(value = "categoryId") Long categoryId, @RequestBody Category categoryObject) {
+        System.out.println("calling updateCategory ==>");
+        return categoryService.updateCategory(categoryId, categoryObject);
+    }
+
+    @DeleteMapping("/categories/{categoryId}")
+    public Optional<Category> deleteCategory(@PathVariable(value = "categoryId") Long categoryId) {
+        System.out.println("calling deleteCategory ==>");
+        return categoryService.deleteCategory(categoryId);
+    }
+
+
 }
