926d4f76101906b943f01eb9e20a3dd55aceb6b7
sophiesucode <sophzsul@gmail.com>
2022-02-27 19:45:56 -0500

    created api endpoints to get, delete,update, and post employer

---
+++ src/main/java/com/jobs/jobs/controller/EmployerController.java
@@ -0,0 +1,54 @@
+package com.jobs.jobs.controller;
+
+
+import com.jobs.jobs.model.Employer;
+import com.jobs.jobs.repository.EmployerRepository;
+import com.jobs.jobs.service.EmployerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class EmployerController {
+
+    private EmployerService employerService;
+
+
+    @Autowired
+    public void setEmployerService(EmployerService employerService) {
+        this.employerService = employerService;
+    }
+
+    public EmployerService getEmployerService() {
+        return employerService;
+    }
+
+
+    @GetMapping(path = "/employers/")
+    public List<Employer> getEmployer() {
+        System.out.println("calling getEmployers ==>");
+        return employerService.getAllEmployers();
+    }
+
+    @GetMapping("/employers/{companyId}")
+    public Optional getEmployers(@PathVariable Long employerId){
+        System.out.println(("calling getAgent ==>"));
+        return employerService.getEmployer(employerId);
+    }
+
+
+    @PostMapping("/employers/")
+    public Employer createEmployer(@RequestBody Employer employerObject) {
+        System.out.println("calling createEmployer ==> ");
+        return employerService.createEmployer(employerObject);
+
+    }
+
+    @DeleteMapping("/jobs/{companyId}")
+    public Optional<Employer> deleteEmployer(@PathVariable(value = "companyId") Long companyId) {
+        System.out.println("calling deleteEmployer ==>");
+        return employerService.deleteEmployer(companyId);
+    }
+}
